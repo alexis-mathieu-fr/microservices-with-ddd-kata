5f833fbeff233bbaeb8de42c73346102c0a32d96
Alexis MATHIEU <alexis.mathieu.p@citeo.com>
2024-09-13 12:03:31 +0200

    initial commit

---
+++ bounded-contexts/dotnet/MyShop.Services/Shipping/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MyShop.Services.Shipping
+{
+    public class Cart
+    {
+        public Cart(IEnumerable<CartItem> items)
+        {
+            Items = items;
+        }
+
+        private IEnumerable<CartItem> Items { get; }
+
+        public IEnumerable<CartItem> GetItems() => Items;
+    }
+}
---
+++ bounded-contexts/dotnet/MyShop.Services/Shipping/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using MyShop.Entities;
+
+namespace MyShop.Services.Shipping
+{
+    public class CartItem
+    {
+        public CartItem(ItemId itemId, int quantity)
+        {
+            ItemId = itemId;
+            Quantity = quantity;
+        }
+
+        public ItemId ItemId { get; }
+        public int Quantity { get; }
+    }
+}
